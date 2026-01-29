06700879160991f83846baff441c7d38cf1ebea3
Finn <36242395+ploorp@users.noreply.github.com>
2026-01-29 10:03:17 -0500

    test(isEmailValid): added more tests

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -27,7 +27,30 @@ class BankAccountTest {
         assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
         assertFalse( BankAccount.isEmailValid(""));         // empty string
 
-        
+        assertFalse(BankAccount.isEmailValid("a.@b.com"));
+        assertFalse(BankAccount.isEmailValid(".a@b.com"));
+        assertFalse(BankAccount.isEmailValid("a..a@b.com"));
+        assertFalse(BankAccount.isEmailValid("ab.com"));
+        assertFalse(BankAccount.isEmailValid("a@b@b.com"));
+        assertFalse(BankAccount.isEmailValid("a"));
+        assertFalse(BankAccount.isEmailValid("@b.com"));
+        assertFalse(BankAccount.isEmailValid("a@"));
+        assertFalse(BankAccount.isEmailValid("a@b.c"));
+        assertFalse(BankAccount.isEmailValid("-a@b.com"));
+        //assertFalse(BankAccount.isEmailValid("a@b.aa")); // technically not a real tld
+        assertFalse(BankAccount.isEmailValid("a@b.co.uk"));
+        assertFalse(BankAccount.isEmailValid("4@4.5"));
+        assertFalse(BankAccount.isEmailValid("a@a#b.com"));
+        assertFalse(BankAccount.isEmailValid("a@ab"));
+        assertFalse(BankAccount.isEmailValid("a@a..com"));
+
+        assertTrue(BankAccount.isEmailValid("a@b.co.uk"));
+        assertTrue(BankAccount.isEmailValid("a@a.b.com"));
+        assertTrue(BankAccount.isEmailValid("a.a@b.com"));
+        assertTrue(BankAccount.isEmailValid("4@b.com"));
+        //assertTrue(BankAccount.isEmailValid("a-@b.com")); // valid according to wikipedia
+        assertTrue(BankAccount.isEmailValid("a+b@b.com"));
+        assertTrue(BankAccount.isEmailValid("A@b.com"));
     }
 
     @Test
