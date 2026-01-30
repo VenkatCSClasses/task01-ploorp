6f69ea52a0e25d73e3b3bd3d16c1fe5dd9f3853f
Liam-Nelsen <lnelsen@ithaca.edu>
2026-01-29 21:33:46 -0500

    added comments to all tests

--- src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
+++ src/test/java/edu/ithaca/dturnbull/bank/BankAccountTest.java
@@ -1,7 +1,10 @@
 package edu.ithaca.dturnbull.bank;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 class BankAccountTest {
@@ -27,23 +30,24 @@ class BankAccountTest {
         assertTrue(BankAccount.isEmailValid( "a@b.com"));   // valid email address
         assertFalse( BankAccount.isEmailValid(""));         // empty string
 
-        assertFalse(BankAccount.isEmailValid("a.@b.com"));
-        assertFalse(BankAccount.isEmailValid(".a@b.com"));
-        assertFalse(BankAccount.isEmailValid("a..a@b.com"));
-        assertFalse(BankAccount.isEmailValid("ab.com"));
-        assertFalse(BankAccount.isEmailValid("a@b@b.com"));
-        assertFalse(BankAccount.isEmailValid("a"));
-        assertFalse(BankAccount.isEmailValid("@b.com"));
-        assertFalse(BankAccount.isEmailValid("a@"));
-        assertFalse(BankAccount.isEmailValid("a@b.c"));
-        assertFalse(BankAccount.isEmailValid("-a@b.com"));
+        assertFalse(BankAccount.isEmailValid("a.@b.com")); //'@'and '.' are next to eachother
+        assertFalse(BankAccount.isEmailValid(".a@b.com")); //starts with '.'
+        assertFalse(BankAccount.isEmailValid("a..a@b.com")); //two '.' in a row
+        assertFalse(BankAccount.isEmailValid("ab.com")); //no '@' symbol
+        assertFalse(BankAccount.isEmailValid("a@b@b.com")); //two '@' symbols
+        assertFalse(BankAccount.isEmailValid("a")); //no '@' symbol, or domain
+        assertFalse(BankAccount.isEmailValid("@b.com")); //starts with '@' symbol
+        assertFalse(BankAccount.isEmailValid("a@")); //ends with '@' symbol
+        assertFalse(BankAccount.isEmailValid("a@b.c")); //domain (after the '.') too short
+        assertFalse(BankAccount.isEmailValid("-a@b.com")); //starts with a character that is not allowed
         //assertFalse(BankAccount.isEmailValid("a@b.aa")); // technically not a real tld
-        assertFalse(BankAccount.isEmailValid("a@b.co.uk"));
-        assertFalse(BankAccount.isEmailValid("4@4.5"));
-        assertFalse(BankAccount.isEmailValid("a@a#b.com"));
-        assertFalse(BankAccount.isEmailValid("a@ab"));
-        assertFalse(BankAccount.isEmailValid("a@a..com"));
+        assertFalse(BankAccount.isEmailValid("a@b.co.uk")); // multiple '.' in domain not allowed
+        assertFalse(BankAccount.isEmailValid("4@4.5")); //domain (after the '.') are not letters
+        assertFalse(BankAccount.isEmailValid("a@a#b.com")); //character '#' is not allowed
+        assertFalse(BankAccount.isEmailValid("a@ab")); //`no '.'
+        assertFalse(BankAccount.isEmailValid("a@a..com")); //two '.' in a row
 
+        //all valid emails addresses
         assertTrue(BankAccount.isEmailValid("a@b.co.uk"));
         assertTrue(BankAccount.isEmailValid("a@a.b.com"));
         assertTrue(BankAccount.isEmailValid("a.a@b.com"));
